f16838e9edc8685bd2d63bcca60fed7630cec550
Amber Williams <awilliams1234@live.com>
2020-02-22 01:54:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,3 +5,9 @@ def key_for_min_value(name_hash)
   name_hash.collect do |name, value|
     smallest value
 end
+
+def key_for_min_value(name_hash)
+  lowest_value = 0
+  lowest_key = nil
+  
+  name_hash.each do |key,value|
